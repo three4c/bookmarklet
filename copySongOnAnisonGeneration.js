f80f78149e9b7f2b6fbdd453574f12174eca39fd
three4c <three4c@gmail.com>
2025-01-19 23:35:07 +0900

    fix copySongOnAnisonGeneration.js

--- copySongOnAnisonGeneration.js
+++ copySongOnAnisonGeneration.js
@@ -17,7 +17,10 @@ export default () => {
   });
 
   const div = document.createElement("div");
-  const onClick = (text) => () => prompt("コピーしてね👇", text);
+  const onClick = (text) => () => {
+    prompt("コピーしてね👇", text);
+    document.body.removeChild(div);
+  };
 
   targets.forEach((item) => {
     const button = document.createElement("button");
@@ -26,5 +29,36 @@ export default () => {
     div.appendChild(button);
   });
 
+  const style = document.createElement("style");
+  style.innerHTML = `
+    div {
+      position: fixed;
+      top: 0;
+      left: 0;
+      z-index: 9999;
+      background: #fff;
+      border-radius: 0 0 8px 8px;
+      box-shadow: 0 0 8px rgba(0, 0, 0, 0.2);
+      display: flex;
+      flex-direction: column;
+      padding: 16px 16px 8px 16px;
+    }
+    
+    button {
+      background: #fff;
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      color: #333;
+      font-size: 14px;
+      margin: 0 8px 8px 0;
+      padding: 4px 8px;
+    }
+
+    button:hover {
+      background: #eee;
+      cursor: pointer;
+    }
+  `;
+  div.appendChild(style);
   document.body.appendChild(div);
 };

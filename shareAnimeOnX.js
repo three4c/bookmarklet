e705b7d5279a7f34ae49b6b66e662c6c1cf0b057
three4c <three4c@gmail.com>
2025-04-04 01:15:41 +0900

    fix YouTube

--- shareAnimeOnX.js
+++ shareAnimeOnX.js
@@ -1,6 +1,15 @@
 export default () => {
   const addBrackets = (text) => (/「(.*?)」/.test(text) ? text : `「${text}」`);
 
+  const youtube = () => {
+    const title = document.title.slice(0, -10);
+    const hashTag = "@YouTube";
+    const text = `${title}を視聴しました！${hashTag}`;
+    const { href } = location;
+    const url = `https://x.com/intent/tweet?text=${encodeURIComponent(text)}&url=${encodeURIComponent(href)}`;
+    return url;
+  };
+
   const dAnimeStore = () => {
     const headerText = document.querySelector("modal .headerText")?.innerText;
     const number = document.querySelector("modal .number")?.innerText;
@@ -33,15 +42,6 @@ export default () => {
     const number = titleSplit[0];
     const hashTag = "@ABEMA";
     const text = `${headerText} ${number}${addBrackets(titleSplit[1])}を視聴しました！${hashTag}`;
-    const { href } = location;
-    const url = `https://x.com/intent/tweet?text=${encodeURIComponent(text)}&url=${encodeURIComponent(href)}`;
-    return url;
-  };
-
-  const youtube = () => {
-    const title = document.title.slice(0, -10);
-    const hashTag = "@YouTube";
-    const text = `${title}を視聴しました！${hashTag}`;
     const { href } = location;
     const url = `https://x.com/intent/tweet?text=${encodeURIComponent(text)}&url=${encodeURIComponent(href)}`;
     return url;
